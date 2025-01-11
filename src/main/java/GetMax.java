5097f1fb9f23f4b0569b11d850bcf461a1fbf00d
ingenerem Marie <ingenerem@gmail.com>
2025-01-11 00:15:56 +0000

    Get max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,17 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
+        if(arr.length!=0){
+
+            int max = arr[0];
+            for(int i=1; i < arr.length; i++){
+                if(arr[i] > max){
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+        
         return -1;
     }
 }
